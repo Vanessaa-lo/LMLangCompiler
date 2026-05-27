c36b2db39a758695de98faf02743feb78717f5b9
Vanessa Loyola <152674387+Vanessaa-lo@users.noreply.github.com>
2026-05-26 22:14:44 -0600

    cambios

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include "parser.h"
 #include "ast.h"
 
 extern ASTNode *astRoot;
 
-int main() {
-    initParser("test_ok.lm");
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Uso: %s <archivo_fuente>\n", argv[0]);
+        return 1;
+    }
+
+    initParser(argv[1]);
     parseProgram();
 
     freeAST(astRoot);
